82b3df5e2ab2a4e963fcdb164141f26011b02d27
Christina537 <34045538+Christina537@users.noreply.github.com>
2017-11-28 15:07:19 -0800

    Adding previous notes
    11.27.17

---
+++ notes.py
@@ -0,0 +1,30 @@
+print("Hello World!")
+
+# Christina
+
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print("See if you can figure this out")
+print(15 % 5)
+
+# Variables
+car_name = "Dragon Beauty"
+car_type = "Volkswagen Van"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+
+# Taking input
+name = input("What is your name?")
+print("Hello %s." % name)
+# print(name)
+age = input("What is your age?")
+print("I'm also %s." % age)
