51ef8aed31baf747c4527d9299f3992523834285
Christina537 <34045538+Christina537@users.noreply.github.com>
2018-01-11 14:48:03 -0800

    1.10.18

---
+++ Lucky 7's.py
@@ -0,0 +1,16 @@
+# Christina Estrada
+
+
+import random
+dic_1 = (random.randint(1, 6))
+dic_2 = (random.randint(1, 6))
+
+total = dic_1 + dic_2
+print("You rolled a", total)
+money = 15
+
+while money > 0:
+    money
+
+
+
