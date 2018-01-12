2baf4d7f364ba08142275e9f71df56493d617552
Christina537 <34045538+Christina537@users.noreply.github.com>
2018-01-12 15:36:53 -0800

    1.12.18

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -6,13 +6,13 @@ import random
 
 money = 15
 played = 0
-
+highest = money
 
 
 
 while money > 0:
-    dic_1 = (random.randint(1, 6))
-    dic_2 = (random.randint(1, 6))
+    dic_1 = (random.randint(1, 7))
+    dic_2 = (random.randint(1, 7))
     print("dic_1 : %s" % dic_1)
     print("dic_2 : %s" % dic_2)
     played += 1
@@ -20,8 +20,10 @@ while money > 0:
     roll = (dic_1 + dic_2)
     if roll == 7:
         money += 4
+        print("Your max is %s" % highest)
     elif roll != 7:
         money -= 1
+        played += 1
 
 if money == 0:
     print("you played %s rounds" % played)
